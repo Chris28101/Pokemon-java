a1cd5c8e3ba4d1b81845d328b90c50d1b2d0c1f1
Chris <christopherekeh4@gmail.com>
2025-10-06 09:37:12 -0400

    first

--- Pokemon.java
+++ Pokemon.java
@@ -8,7 +8,8 @@ public class Pokemon {
     int speed;
     int SpAttack;
     int SpDefence;
-    //Defult constructor
+    int level;
+    //Default constructor
     public Pokemon(){
         name = "";
         health = 0;
@@ -18,11 +19,89 @@ public class Pokemon {
         speed = 0;
         SpAttack = 0;
         SpDefence = 0;
+        level = 1;
     }
-    public Pokemon(String name, int health, String type){
+    //parameter constructor
+    public Pokemon(String name, int health, String type,int speed, int attack , int defence , int SpAttack, int SpDefence, int level){
         this.name = name;
         this.health = health;
         this.type = type;
+        this.speed = speed;
+        this.attack = attack;
+        this.defence = defence;
+        this.SpDefence = SpDefence;
+        this.SpAttack = SpAttack;
+        this.level = level;
+
+    }
+    //getters and setters
+    public void setName(String Name) {
+        name = Name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setType(String Type) {
+        type = Type;
+    }
+    public String getType(){
+        return type;
+    }
+    public void setHealth(int Health){
+        health = Health;
+    }
+    public int getHealth(){
+       return health;
+    }
+
+    public void setAttack(int Attack) {
+        attack = Attack;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setDefence(int Defence) {
+        defence = Defence;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setSpeed(int Speed) {
+        speed = Speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setLevel(int level1) {
+        level = level1;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setSpAttack(int spAttack) {
+        SpAttack = spAttack;
+    }
+
+    public int getSpAttack() {
+        return SpAttack;
+    }
+
+    public void setSpDefence(int spDefence) {
+        SpDefence = spDefence;
+    }
+
+    public int getSpDefence() {
+        return SpDefence;
     }
 
 }
